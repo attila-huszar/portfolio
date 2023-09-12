444174ad8c0e8c778a7e73fd4bdb187018df1eed
Attila Huszár <attila.huszar@outlook.com>
2023-09-13 00:59:01 +0200

    Upd favicon

--- src/components/PendingFetch.jsx
+++ src/components/PendingFetch.jsx
@@ -3,7 +3,7 @@ import ringsLoader from "../assets/rings.svg";
 export function PendingFetch() {
   return (
     <div style={{ width: "100%" }}>
-      <img src={ringsLoader} style={{ margin: "auto" }} alt="Loading" />
+      <img src={ringsLoader} style={{ margin: "auto", width: "45px", height: "45px" }} />
       <p style={{ textAlign: "center" }}>Loading...</p>
     </div>
   );
