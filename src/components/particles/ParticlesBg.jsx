eee227758c9d6766fbd18362e04155d7a15accf2
Attila Huszár <attila.huszar@outlook.com>
2023-12-09 16:16:25 +0100

    Bump pkgs, upd tsparticles

--- src/components/particles/ParticlesBg.jsx
+++ src/components/particles/ParticlesBg.jsx
@@ -1,11 +1,19 @@
-import Particles from "react-particles";
-import { loadSlim } from "tsparticles-slim";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+//import { loadBasic } from "@tsparticles/basic";
 import { particleBgColors } from "./particleBgColors";
-import { useCallback, useEffect, useState } from "react";
 
 export default function ParticlesBg({ darkMode }) {
-  const particlesInit = useCallback(async engine => {
-    await loadSlim(engine);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async engine => {
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
   const { background, links, particle } = particleBgColors();
@@ -20,89 +28,93 @@ export default function ParticlesBg({ darkMode }) {
   }, [darkMode]);
 
   return (
-    <Particles
-      init={particlesInit}
-      options={{
-        background: {
-          color: {
-            value: backgroundColor,
-          },
-        },
-        fpsLimit: 60,
-        fullScreen: {
-          enable: true,
-          zIndex: -1,
-        },
-        interactivity: {
-          detectsOn: "window",
-          events: {
-            onClick: {
+    <>
+      {init && (
+        <Particles
+          id="tsparticles"
+          options={{
+            background: {
+              color: {
+                value: backgroundColor,
+              },
+            },
+            fpsLimit: 60,
+            fullScreen: {
               enable: true,
-              mode: "push",
+              zIndex: -1,
             },
-            onHover: {
-              enable: true,
-              mode: "repulse",
+            interactivity: {
+              detectsOn: "window",
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 1,
+                },
+                repulse: {
+                  distance: 150,
+                  duration: 0.4,
+                },
+              },
             },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 1,
+            particles: {
+              color: {
+                value: particleColor,
+              },
+              links: {
+                color: linksColor,
+                distance: 150,
+                enable: true,
+                opacity: 0.7,
+                width: 2,
+                triangles: {
+                  enable: true,
+                  color: linksColor,
+                  opacity: 0.2,
+                },
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: true,
+                speed: 3,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 2000,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.9,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: { min: 3, max: 5 },
+              },
             },
-            repulse: {
-              distance: 150,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: particleColor,
-          },
-          links: {
-            color: linksColor,
-            distance: 150,
-            enable: true,
-            opacity: 0.7,
-            width: 2,
-            triangles: {
-              enable: true,
-              color: linksColor,
-              opacity: 0.2,
-            },
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: true,
-            speed: 3,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 2000,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.9,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: { min: 3, max: 5 },
-          },
-        },
-        detectRetina: true,
-      }}
-    />
+            detectRetina: true,
+          }}
+        />
+      )}
+    </>
   );
 }
