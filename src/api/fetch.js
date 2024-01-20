231957647b2968cd446bb4cbcb6437b54fa55d53
Attila Huszár <attila.huszar@outlook.com>
2024-01-20 23:07:51 +0100

    separate fetch api

---
+++ src/api/fetch.js
@@ -0,0 +1,12 @@
+const INFO_URL =
+  'https://s3.eu-central-1.amazonaws.com/attila.huszar/portfolio/info.json'
+
+export async function fetchData() {
+  try {
+    const response = await fetch(INFO_URL)
+    const result = await response.json()
+    return result
+  } catch (error) {
+    return error
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,24 @@ import { BrowserRouter } from 'react-router-dom'
 import { useState, useEffect, createContext } from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import { useDarkMode } from 'usehooks-ts'
+import { fetchData } from '../api/fetch'
 import Layout from './Layout'
-import dataFallback from '../assets/info_fallback.json'
+import infoFallback from '../assets/info_fallback.json'
 
-export const DataContext = createContext(dataFallback)
+export const DataContext = createContext(infoFallback)
+const info = await fetchData()
 
 export default function App() {
   const { isDarkMode } = useDarkMode()
   const [data, setData] = useState()
 
   useEffect(() => {
-    fetchData()
-  }, [])
-
-  async function fetchData() {
-    try {
-      const response = await fetch(
-        'https://s3.eu-central-1.amazonaws.com/attila.huszar/portfolio/info.json',
-      )
-      const result = await response.json()
-      setData(result)
-    } catch (error) {
-      setData(dataFallback)
+    if (info instanceof Error || !info) {
+      setData(infoFallback)
+    } else {
+      setData(info)
     }
-  }
+  }, [])
 
   const light = {
     palette: {
