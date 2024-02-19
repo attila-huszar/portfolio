93f8a53a6ef7695a9cae2c94a3d9bc3781a7c803
Attila Huszár <attila.huszar@outlook.com>
2024-02-19 15:26:32 +0100

    upd config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: true,
+}
+
+export default config
