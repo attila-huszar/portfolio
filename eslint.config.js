d568a91ceff74c13f730b35554e5b049badf4ba8
Attila Huszár <attila.huszar@outlook.com>
2025-04-06 00:40:03 +0200

    remove unused eslint-plugin-import-x and related configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
 import eslint from '@eslint/js'
 import react from 'eslint-plugin-react'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import importX from 'eslint-plugin-import-x'
 import globals from 'globals'
 import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 
 export default [
   eslint.configs.recommended,
-  importX.flatConfigs.recommended,
   react.configs.flat.recommended,
   prettierConfig,
   {
@@ -32,17 +30,11 @@ export default [
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
-      'import-x/no-unresolved': ['error', { ignore: ['.svg?'] }],
       'prettier/prettier': 'warn',
     },
   },
   {
     settings: {
-      'import-x/resolver': {
-        node: {
-          extensions: ['.js', '.jsx'],
-        },
-      },
       react: {
         version: 'detect',
       },
